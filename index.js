62807990a4779bea8c0ecf283286f48d423bbb55
CodingAleCR <aulate@soin.co.cr>
2018-08-08 15:01:33 -0600

    Lecture 22

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/google-keys");
 
 const app = express();
 
 // clientID  168727507555-nsmt6cssabd7q0kc7on43g7mh73eu3hc.apps.googleusercontent.com
 // clientSecret R3aUgJxKJS2iOsdM_HO3CXqE
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
