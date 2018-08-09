a06d3700a59def89d8e52b706b9f4494ee312da7
CodingAleCR <aulate@soin.co.cr>
2018-08-09 14:23:55 -0600

    Lecture 33

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const mongoKeys = require("./config/mongodb-keys");
 require("./services/passport");
+
+mongoose.connect(mongoKeys.mongoURI);
 
 const app = express();
 
